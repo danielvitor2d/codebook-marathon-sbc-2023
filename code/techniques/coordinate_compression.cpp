0bb6a9349d97e95fb5af38dda58381277fd85c13
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-08-19 18:38:47 -0300

    Add coordinate compression

---
+++ code/techniques/coordinate_compression.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace CoordinateCompression {
+    // Runs in O(n log n)
+    void coordinate_compression(vector<int> &v) {
+	    vector<int> aux(v);
+
+	    sort(aux.begin(), aux.end());
+
+	    unordered_map<int, int> ump;
+	    int cnt {1};
+
+	    for (int x : aux) {
+		    ump[x] = cnt++;
+	    }
+
+	    for (int &x : v) {
+		    x = ump[x];
+	    }
+    }
+}
